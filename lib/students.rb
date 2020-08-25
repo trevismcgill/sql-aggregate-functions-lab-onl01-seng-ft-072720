d3a888585cb192f78830e26c690731b0fe325cde
Trevis McGill <trevismcgill25@gmail.com>
2020-08-25 00:08:22 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,15 +1,15 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  
+
 end
 
 def lowest_student_gpa
-  
+
 end
 
 def average_student_gpa
-  
+
 end
 
 def total_tardies_for_all_students
